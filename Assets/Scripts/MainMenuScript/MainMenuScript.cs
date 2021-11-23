8bcc6ea658500438e8c79224fc85c6c822145af5
ItsRifter <d_thomas_smith30@hotmail.com>
2021-11-23 13:25:33 +0000

    music

--- Assets/Scripts/MainMenuScript/MainMenuScript.cs
+++ Assets/Scripts/MainMenuScript/MainMenuScript.cs
@@ -2,12 +2,46 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenuScript : MonoBehaviour
 {
+
+    private GameObject mainMenu;
+    private GameObject optionsMenu;
+
+    private void Awake()
+    {
+        mainMenu = GameObject.Find("MainMenu");
+        optionsMenu = GameObject.Find("OptionsPanel");
+    }
+
+    private void Start()
+    {
+        mainMenu.SetActive(true);
+        optionsMenu.SetActive(false);
+    }
+
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void OpenOptions()
+    {
+        mainMenu.SetActive(false);
+        optionsMenu.SetActive(true);
+    }
+
+    public void ApplyChanges()
+    {
+        
+    }
+
+    public void ReturnToMainMenu()
+    {
+        mainMenu.SetActive(true);
+        optionsMenu.SetActive(false);
     }
 
     public void QuitGame()
