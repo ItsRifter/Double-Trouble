b2c12c2cf86269a4a840d6cf0447bcba4c9820e5
Mckinlaydnl <43002081+Mckinlaydnl@users.noreply.github.com>
2021-11-24 00:47:46 +0000

    Made the level look nicer and added two buttons which end the game when both players are near their respective one.

---
+++ Assets/Scripts/EndGameButtonScriptPlayer1.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndGameButtonScriptPlayer1 : MonoBehaviour
+{
+    public EndGameButtonScriptPlayer2 EGBSP2;
+    public bool isPlayer1Near = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.collider.tag == "Player")
+        {
+            Debug.Log("Player Is Near Button");
+            isPlayer1Near = true;
+        }
+        if (EGBSP2.isPlayer2Near)
+        {
+            Debug.Log("End Game");
+        }
+        
+
+
+    }
+
+    void OnCollisionExit(Collision col)
+    {
+            isPlayer1Near = false;   
+    }
+
+}
+
+
+
---
+++ Assets/Scripts/EndGameButtonScriptPlayer2.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndGameButtonScriptPlayer2 : MonoBehaviour
+
+{
+    public EndGameButtonScriptPlayer1 EGBSP1;
+    public bool isPlayer2Near = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.collider.tag == "Player")
+        {
+            Debug.Log("Player Is Near Button");
+            isPlayer2Near = true;
+
+        }
+        if (EGBSP1.isPlayer1Near)
+        {
+            Debug.Log("End Game");
+        }
+
+
+    }
+
+    void OnCollisionExit(Collision col)
+    {
+        isPlayer2Near = false;
+    }
+     
+
+
+
+}
